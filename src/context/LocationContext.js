2b82516365fa4ec34e9db86d469a50691a2b2b10
michaldobiezynski <dobiezynski@gmail.com>
2020-07-08 09:01:34 +0100

    clean up after ourselves

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -5,7 +5,7 @@ const locationReducer = (state, action) => {
     case "add_current_location":
       return { ...state, currentLocation: action.payload };
     case "add_location":
-      return { ...state, locations: [...state.location, action.payload] };
+      return { ...state, locations: [...state.locations, action.payload] };
     case "start_recording":
       return { ...state, recording: true };
     case "stop_recording":
--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -37,7 +37,13 @@ export default (shouldTrack, callback) => {
       subscriber.remove();
       setSubscriber(null);
     }
-  }, [shouldTrack]);
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback]);
 
   return [err];
 };
